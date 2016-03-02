2f5f0e6f14afec8c40f56eb18eb6aba71abf6bd0
rahulsood03 <rahulsood@Rahuls-MacBook-Air.local>
2016-03-02 13:42:58 -0500

    first commit

---
+++ code1.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>
+My First Program
+</title>
+</head>
+<body>
+    <h2>code1</h2>
+    
+<?php
+echo 
+"<h3>"
+.
+"This is my first program"
+.
+"</h3>"
+;
+echo 
+"<h5>"
+.
+"I am trying to print a string in the browser window"
+.
+"</h5>"
+;
+?>
+    
+    
+</body>
+</html>
